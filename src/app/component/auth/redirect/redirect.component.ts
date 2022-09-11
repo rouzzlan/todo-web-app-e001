3c5efa6016ebfd6abcb90de36132c1c38f098ab8
Rouslan Khayaouri <rouslan_kh@hotmail.com>
2022-09-11 21:08:03 +0300

    base login tested

--- src/app/component/auth/redirect/redirect.component.ts
+++ src/app/component/auth/redirect/redirect.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from "../../../service/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-redirect',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./redirect.component.css']
 })
 export class RedirectComponent implements OnInit {
-
-  constructor() { }
+  constructor(private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    if (this.router.url.startsWith('/login-redirect')) {
+      this.authService.completeLogin().then(() => {
+        this.router.navigate(['/home'], {replaceUrl: true}).then();
+      });
+    } else {
+      this.authService.completeLogout().then(() => {
+        this.router.navigate(['/'], {replaceUrl: true}).then();
+      });
+    }
   }
-
 }
--- src/app/component/public/welcome/welcome.component.ts
+++ src/app/component/public/welcome/welcome.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../../service/auth.service";
 
 @Component({
@@ -8,7 +8,8 @@ import {AuthService} from "../../../service/auth.service";
 })
 export class WelcomeComponent implements OnInit {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService) {
+  }
 
   ngOnInit(): void {
   }
